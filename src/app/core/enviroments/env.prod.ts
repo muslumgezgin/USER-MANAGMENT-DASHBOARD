70699ef0b87d8b66e7cade9b6ab0b1246c57729a
muslumgezgin <muslum_gezgin@hotmail.com>
2023-05-25 19:25:29 +0200

    add enviroment config

---
+++ src/app/core/enviroments/env.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    configurationUrl: '/public/configuration.json'
+  };
---
+++ src/app/core/enviroments/env.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    configurationUrl: '/public/configuration.json'
+  };
